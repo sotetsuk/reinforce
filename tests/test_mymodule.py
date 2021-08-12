dded01eb0e6c8253ae6d0b925919c7d5b489afa7
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-12 16:55:45 +0900

    Remove dummy implementations (#3)

--- tests/test_mymodule.py
+++
@@ -1,8 +0,0 @@
-from reinforce.mymodule import fizzbuzz
-
-
-def test_fizzbuzz() -> None:
-    assert fizzbuzz(1) == "1"
-    assert fizzbuzz(3) == "Fizz"
-    assert fizzbuzz(5) == "Buzz"
-    assert fizzbuzz(15) == "FizzBuzz"
