c1d4331fd24631c632f750f0212b0465bd10562a
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-13 20:46:30 +0900

    Implement BatchAvgBaselineMixin (#8)
    * add baseline mixin
    
    * rename
    
    * revert main

--- reinforce/__init__.py
+++ reinforce/__init__.py
@@ -1,4 +1,14 @@
-from reinforce.reinforce import REINFORCE
+from reinforce.reinforce import (
+    REINFORCE,
+    BatchAvgBaselineMixin,
+    FutureRewardMixin,
+)
 from reinforce.vector_env import EpisodicAsyncVectorEnv, EpisodicSyncVectorEnv
 
-__all__ = ["REINFORCE", "EpisodicAsyncVectorEnv", "EpisodicSyncVectorEnv"]
+__all__ = [
+    "REINFORCE",
+    "FutureRewardMixin",
+    "BatchAvgBaselineMixin",
+    "EpisodicAsyncVectorEnv",
+    "EpisodicSyncVectorEnv",
+]
