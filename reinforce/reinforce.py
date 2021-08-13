19bf71286b4248e2d4528a553c4ca470b5084629
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-13 11:21:50 +0900

    refactor

--- reinforce/reinforce.py
+++ reinforce/reinforce.py
@@ -10,22 +10,23 @@ cross_entropy = nn.CrossEntropyLoss(reduction="none")
 
 
 class REINFORCE:
-    def __init__(self):
-        self.gamma = 1.0
-        self.rewards: List[torch.Tensor] = []
-        self.actions: List[torch.Tensor] = []
-        self.logits: List[torch.Tensor] = []
-        self.masks: List[torch.Tensor] = []
+    def __init__(self, gamma=1.0):
+        self.gamma = gamma  # TODO: use gamma
         self.n_steps: int = 0
         self.n_episodes: int = 0
         self.n_batch_updates: int = 0
+
+        self._rewards_seq: List[torch.Tensor] = []
+        self._action_seq: List[torch.Tensor] = []
+        self._logits_seq: List[torch.Tensor] = []
+        self._masks_seq: List[torch.Tensor] = []
 
     def compute_baseline(self):
         return 0
 
     def compute_return(self):
         R = (
-            torch.stack(self.rewards)
+            torch.stack(self._rewards_seq)
             .t()
             .flip(dims=(1,))
             .cumsum(dim=1)
@@ -35,7 +36,7 @@ class REINFORCE:
 
     def compute_neg_log_prob(self):
         neg_log_probs = []
-        for logit, a in zip(self.logits, self.actions):
+        for logit, a in zip(self._logits_seq, self._action_seq):
             neg_log_probs.append(cross_entropy(logit, a))
         return torch.stack(neg_log_probs).t()  # (n_env, max_seq_len)
 
@@ -43,20 +44,20 @@ class REINFORCE:
         return ((R - b) * neg_log_prob * mask).sum(dim=1).mean(dim=0)
 
     def push_data(self, logits, actions, rewards, dones):
-        self.rewards.append(torch.from_numpy(rewards).float())
-        self.actions.append(actions)
-        self.logits.append(logits)
-        self.masks.append(1.0 - torch.from_numpy(dones).float())
+        self._rewards_seq.append(torch.from_numpy(rewards).float())
+        self._action_seq.append(actions)
+        self._logits_seq.append(logits)
+        self._masks_seq.append(1.0 - torch.from_numpy(dones).float())
 
     def update_gradient(self, optimizer: optim.Optimizer):
         self.n_batch_updates += 1
         optimizer.zero_grad()
 
         # make batch
-        R = self.compute_return()
-        neg_log_prob = self.compute_neg_log_prob()
+        R = self.compute_return()  # (n_env, max_seq_len)
+        neg_log_prob = self.compute_neg_log_prob()  # (n_env, max_seq_len)
         b = self.compute_baseline()
-        mask = torch.stack(self.masks).t()  # (n_env, max_seq_len)
+        mask = torch.stack(self._masks_seq).t()  # (n_env, max_seq_len)
 
         # calculate loss
         loss = self.compute_loss(R, b, neg_log_prob, mask)
@@ -65,10 +66,10 @@ class REINFORCE:
         loss.backward()
         optimizer.step()
 
-        self.rewards = []
-        self.actions = []
-        self.logits = []
-        self.masks = []
+        self._rewards_seq = []
+        self._action_seq = []
+        self._logits_seq = []
+        self._masks_seq = []
 
     def train(
         self,
@@ -82,18 +83,18 @@ class REINFORCE:
         n_steps = 0
         while n_steps < n_steps_limit:
             self.n_episodes += num_envs
-            obs = env.reset()  # shape = (num_envs, obs_dim)
+            observations = env.reset()  # shape = (num_envs, obs_dim)
             dones = [False for _ in range(num_envs)]
             while not all(dones):
                 logits = model(
-                    torch.from_numpy(obs).float()
+                    torch.from_numpy(observations).float()
                 )  # shape = (num_envs, action_dim)
-                a = Categorical(
+                actions = Categorical(
                     logits=logits
                 ).sample()  # shape = (num_envs, action_dim)
                 n_steps += sum([not done for done in dones])
                 self.n_steps += sum([not done for done in dones])
-                obs, r, dones, info = env.step(a.numpy())
-                self.push_data(logits, a, r, dones)
+                observations, rewards, dones, info = env.step(actions.numpy())
+                self.push_data(logits, actions, rewards, dones)
 
             self.update_gradient(optimizer)
