c5749aea4708d8de63638aaa7012d2be0fb385c8
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-13 16:37:11 +0900

    refactor

--- reinforce/reinforce.py
+++ reinforce/reinforce.py
@@ -21,62 +21,6 @@ class REINFORCE:
         self._logits_seq: List[torch.Tensor] = []
         self._masks_seq: List[torch.Tensor] = []
 
-    def compute_baseline(self):
-        return 0
-
-    def compute_return(self):
-        R = (
-            torch.stack(self._rewards_seq)
-            .t()
-            .flip(dims=(1,))
-            .cumsum(dim=1)
-            .flip(dims=(1,))
-        )
-        return R  # (n_env, max_seq_len)
-
-    def compute_neg_log_prob(self):
-        seq_len = len(self._logits_seq)
-        logits = torch.cat(
-            self._logits_seq
-        )  # (num_envs * max_seq_len, num_actions)
-        actions = torch.cat(self._action_seq)  # (num_envs * max_seq_len)
-        neg_log_probs = cross_entropy(
-            logits, actions
-        )  # (num_envs * max_seq_len)
-        neg_log_probs = torch.reshape(neg_log_probs, (seq_len, -1)).t()
-        return neg_log_probs  # (num_envs, max_seq_len)
-
-    def compute_loss(self, R, b, neg_log_prob, mask):
-        return ((R - b) * neg_log_prob * mask).sum(dim=1).mean(dim=0)
-
-    def push_data(self, logits, actions, rewards, dones):
-        self._rewards_seq.append(torch.from_numpy(rewards).float())
-        self._action_seq.append(actions)
-        self._logits_seq.append(logits)
-        self._masks_seq.append(1.0 - torch.from_numpy(dones).float())
-
-    def update_gradient(self, optimizer: optim.Optimizer):
-        self.n_batch_updates += 1
-        optimizer.zero_grad()
-
-        # make batch
-        R = self.compute_return()  # (num_env, max_seq_len)
-        neg_log_prob = self.compute_neg_log_prob()  # (num_env, max_seq_len)
-        b = self.compute_baseline()
-        mask = torch.stack(self._masks_seq).t()  # (num_env, max_seq_len)
-
-        # calculate loss
-        loss = self.compute_loss(R, b, neg_log_prob, mask)
-
-        # update grad
-        loss.backward()
-        optimizer.step()
-
-        self._rewards_seq = []
-        self._action_seq = []
-        self._logits_seq = []
-        self._masks_seq = []
-
     def train(
         self,
         env: VectorEnv,
@@ -101,6 +45,62 @@ class REINFORCE:
                 n_steps += sum([not done for done in dones])
                 self.n_steps += sum([not done for done in dones])
                 observations, rewards, dones, info = env.step(actions.numpy())
-                self.push_data(logits, actions, rewards, dones)
+                self._push_data(logits, actions, rewards, dones)
 
             self.update_gradient(optimizer)
+
+    def update_gradient(self, optimizer: optim.Optimizer):
+        self.n_batch_updates += 1
+        optimizer.zero_grad()
+
+        # make batch
+        R = self.compute_return()  # (num_env, max_seq_len)
+        neg_log_prob = self.compute_neg_log_prob()  # (num_env, max_seq_len)
+        b = self.compute_baseline()
+        mask = torch.stack(self._masks_seq).t()  # (num_env, max_seq_len)
+
+        # calculate loss
+        loss = self.compute_loss(R, b, neg_log_prob, mask)
+
+        # update grad
+        loss.backward()
+        optimizer.step()
+
+        self._rewards_seq = []
+        self._action_seq = []
+        self._logits_seq = []
+        self._masks_seq = []
+
+    def compute_return(self):
+        R = (
+            torch.stack(self._rewards_seq)
+            .t()
+            .flip(dims=(1,))
+            .cumsum(dim=1)
+            .flip(dims=(1,))
+        )
+        return R  # (n_env, max_seq_len)
+
+    def compute_baseline(self):
+        return 0
+
+    def compute_neg_log_prob(self):
+        seq_len = len(self._logits_seq)
+        logits = torch.cat(
+            self._logits_seq
+        )  # (num_envs * max_seq_len, num_actions)
+        actions = torch.cat(self._action_seq)  # (num_envs * max_seq_len)
+        neg_log_probs = cross_entropy(
+            logits, actions
+        )  # (num_envs * max_seq_len)
+        neg_log_probs = torch.reshape(neg_log_probs, (seq_len, -1)).t()
+        return neg_log_probs  # (num_envs, max_seq_len)
+
+    def compute_loss(self, R, b, neg_log_prob, mask):
+        return ((R - b) * neg_log_prob * mask).sum(dim=1).mean(dim=0)
+
+    def _push_data(self, logits, actions, rewards, dones):
+        self._rewards_seq.append(torch.from_numpy(rewards).float())
+        self._action_seq.append(actions)
+        self._logits_seq.append(logits)
+        self._masks_seq.append(1.0 - torch.from_numpy(dones).float())
