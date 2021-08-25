d4ef8efd81b719f7f6e8021111b3e217ff44bb92
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2021-08-25 14:33:26 +0900

    add entropy lo ss mixin

--- reinforce/__init__.py
+++ reinforce/__init__.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2021 Sotetsu KOYAMADA
 # https://github.com/sotetsuk/reinforce/blob/master/LICENSE
 
-from reinforce.mixin import BatchAvgBaselineMixin, FutureRewardMixin
+from reinforce.mixin import (
+    BatchAvgBaselineMixin,
+    EntLossMixin,
+    FutureRewardMixin,
+)
 from reinforce.reinforce import REINFORCE
 from reinforce.vector_env import EpisodicAsyncVectorEnv, EpisodicSyncVectorEnv
 
@@ -9,6 +13,7 @@ __all__ = [
     "REINFORCE",
     "FutureRewardMixin",
     "BatchAvgBaselineMixin",
+    "EntLossMixin",
     "EpisodicAsyncVectorEnv",
     "EpisodicSyncVectorEnv",
 ]
